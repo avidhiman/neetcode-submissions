c79e55612a254a6a887b28866232185d3cd3f028
avidhiman <48544390+avidhiman@users.noreply.github.com>
2026-06-01 21:15:47 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} newInterval
+     * @return {number[][]}
+     */
+    insert(intervals, newInterval) {
+        const res = []
+        for(let interval of intervals) {
+            if(interval[1] < newInterval[0]){
+                res.push(interval)
+            } else if (interval[0] > newInterval[1]) {
+                res.push(newInterval)
+                newInterval = interval
+            } else {
+                newInterval[0] = Math.min(newInterval[0], interval[0])
+                newInterval[1] = Math.max(newInterval[1], interval[1])
+            }
+        }
+        res.push(newInterval);
+        return res
+    }
+}
